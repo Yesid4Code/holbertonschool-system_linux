ce9c62741829ac26d057eef61d8399aaea28baa5
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-12-31 13:03:48 -0500

    functions moved to another file

--- 0x00-ls/auxiliar_functions.c
+++ 0x00-ls/auxiliar_functions.c
@@ -1,42 +1,6 @@
 #include "headers_ls.h"
 
 /**
- * _calloc - function that reserved memory and fill it with zeros.
- * @sizepptr: number of strings inside the array line.
- * @sizetype: type of data
- *
- * Return: array filled with zeros.
- */
-void *_calloc(unsigned int sizepptr, unsigned int sizetype)
-{
-	char *argline = NULL;
-
-	if (sizepptr == 0 || sizetype == 0)
-		return (NULL);
-	argline = malloc(sizepptr * sizetype + 1);
-	if (!argline)
-		return (NULL);
-	argline = _memset(argline, 0, (sizepptr * sizetype));
-	return (argline);
-}
-
-/**
- * _memset - function that inicializate each character of the string with zero
- * @str: str
- * @a: Char to fill the string, a = "0"
- * @size: lenght of the string
- * Return: A pointer to string
- */
-char *_memset(char *str, char a, unsigned int size)
-{
-	unsigned int i;
-
-	for (i = 0; i < size; i++)
-		str[i] = a;
-	return (str);
-}
-
-/**
  * include - function that search a character in a string.
  * @string: pointer to string.
  * @arg: character to found in string.
@@ -105,29 +69,4 @@ int _strcmp(char *s1, char *s2)
 		i++;
 	}
 	return (j);
-}
-
-/**
- * _strdup - Function that return a pointer to duplicate string.
- * @str: pointer to a string.
- *
- * Return: Pointer to an string.
- */
-char *_strdup(char *str)
-{
-	int i = 0, j;
-	char *la;
-
-	if (str == NULL)
-		return (NULL);
-
-	i = _strlen(str) + 1;
-	la = malloc(i * sizeof(char)); /*reserve the space of memory*/
-	if (!la)
-		return (NULL);
-
-	for (j = 0; j < i; j++)
-		la[j] = str[j];
-
-	return (la);
 }
---
+++ 0x00-ls/memory_functions.c
@@ -0,0 +1,72 @@
+#include "headers_ls.h"
+
+/**
+ * _calloc - function that reserved memory and fill it with zeros.
+ * @sizepptr: number of strings inside the array line.
+ * @sizetype: type of data
+ *
+ * Return: array filled with zeros.
+ */
+void *_calloc(unsigned int sizepptr, unsigned int sizetype)
+{
+	char *argline = NULL;
+
+	if (sizepptr == 0 || sizetype == 0)
+		return (NULL);
+	argline = malloc(sizepptr * sizetype);
+	if (!argline)
+		return (NULL);
+	argline = _memset(argline, 0, (sizepptr * sizetype));
+	return (argline);
+}
+
+/**
+ * _strdup - Function that return a pointer to duplicate string.
+ * @str: pointer to a string.
+ *
+ * Return: Pointer to an string.
+ */
+char *_strdup(char *str)
+{
+	int i = 0, j;
+	char *la;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = _strlen(str) + 1;
+	la = malloc(i * sizeof(*str) + 1); /*reserve the space of memory*/
+	if (!la)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		la[j] = str[j];
+    la[j] = '\0';
+	return (la);
+}
+
+/**
+ * _memset - function that inicializate each character of the string with zero
+ * @str: str
+ * @a: Char to fill the string, a = "0"
+ * @size: lenght of the string
+ * Return: A pointer to string
+ */
+char *_memset(char *str, char a, unsigned int size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		str[i] = a;
+	return (str);
+}
+
+void free_array(char **argv)
+{
+    if(argv)
+    {
+        for(int i=0; argv[i]; i++)
+            free(argv[i]);
+        free(argv);
+    }
+}
